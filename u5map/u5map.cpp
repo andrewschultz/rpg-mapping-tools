c3a38aa2ed3d5df62705efa93dfdc8747e1aec0e
andrewschultz <schultz.andrew@sbcglobal.net>
2015-05-03 13:31:04 -0500

    fixed checkbox bug, started u5 map

---
+++ u5map/u5map.cpp
@@ -0,0 +1,376 @@
+// u5map.cpp : Defines the entry point for the application.
+//
+
+#define WIN32_LEAN_AND_MEAN  
+
+#include <ddraw.h>
+#include <windowsx.h>
+#include <stdio.h>
+#include <math.h>
+#include <commdlg.h>
+
+#include <stdlib.h>
+
+#include "u5map.h"
+
+#define ICONSIZE 32
+
+//#defines for Windows
+#define WINDOW_CLASS_NAME "WINCLASS1"
+
+// functions
+void ReadTheDungeons();
+void PaintDunMap();
+void PaintRoomMap();
+short curDirValid();
+void adjustSecretCheckmarks();
+
+//globals
+long curRoom = 0;
+long curDungeon = 0;
+long curLevel = 0;
+
+long resetRoomA = 0;
+
+short wrapHalf = 0;
+
+short dunTile[8][8][8][8];
+short roomBase[11][11][16][8];
+
+HWND hwnd;
+
+HDC roomdc;
+HDC leveldc;
+HDC level2dc;
+
+HDC localhdc;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, 
+						    UINT msg, 
+                            WPARAM wparam, 
+                            LPARAM lparam)
+{
+	short temp;
+
+	switch(msg)
+	{
+	case WM_COMMAND:
+		switch(LOWORD(wparam))
+		{
+		case ID_DUNGEON_DECEIT:
+		case ID_DUNGEON_DESPISE:
+		case ID_DUNGEON_DESTARD:
+		case ID_DUNGEON_WRONG:
+		case ID_DUNGEON_COVETOUS:
+		case ID_DUNGEON_SHAME:
+		case ID_DUNGEON_HYTHLOTH:
+		case ID_DUNGEON_DOOM:
+			if (curDungeon != LOWORD(wparam) - ID_DUNGEON_DECEIT)
+			{
+				CheckMenuItem( GetMenu(hwnd), ID_DUNGEON_DECEIT + curDungeon, MF_UNCHECKED);
+				curDungeon = LOWORD(wparam) - ID_DUNGEON_DECEIT;
+				CheckMenuItem( GetMenu(hwnd), ID_DUNGEON_DECEIT + curDungeon, MF_CHECKED);
+				PaintDunMap();
+				if (resetRoomA)
+					curRoom = 0;
+				PaintRoomMap();
+			}
+			break;
+
+		case ID_DUNGEON_PREV:
+			if (curDungeon > 0)
+			{
+				CheckMenuItem( GetMenu(hwnd), ID_DUNGEON_DECEIT + curDungeon, MF_UNCHECKED);
+				curDungeon--;
+				CheckMenuItem( GetMenu(hwnd), ID_DUNGEON_DECEIT + curDungeon, MF_CHECKED);
+				PaintDunMap();
+				if (resetRoomA)
+					curRoom = 0;
+				PaintRoomMap();
+			}
+			break;
+
+		case ID_DUNGEON_NEXT:
+			if (curDungeon < 7)
+			{
+				CheckMenuItem( GetMenu(hwnd), ID_DUNGEON_DECEIT + curDungeon, MF_UNCHECKED);
+				curDungeon++;
+				CheckMenuItem( GetMenu(hwnd), ID_DUNGEON_DECEIT + curDungeon, MF_CHECKED);
+				PaintDunMap();
+				if (resetRoomA)
+					curRoom = 0;
+				PaintRoomMap();
+			}
+			break;
+
+		case ID_NAV_1:
+		case ID_NAV_2:
+		case ID_NAV_3:
+		case ID_NAV_4:
+		case ID_NAV_5:
+		case ID_NAV_6:
+		case ID_NAV_7:
+		case ID_NAV_8:
+			if (curLevel != LOWORD(wparam)-ID_NAV_1)
+			{
+				CheckMenuItem( GetMenu(hwnd), ID_NAV_1 + curLevel, MF_UNCHECKED);
+				curLevel = LOWORD(wparam) - ID_NAV_1;
+				CheckMenuItem( GetMenu(hwnd), ID_NAV_1 + curLevel, MF_CHECKED);
+				PaintDunMap();
+			}
+			break;
+
+		case ID_NAV_A:
+		case ID_NAV_B:
+		case ID_NAV_C:
+		case ID_NAV_D:
+		case ID_NAV_E:
+		case ID_NAV_F:
+		case ID_NAV_G:
+		case ID_NAV_H:
+		case ID_NAV_I:
+		case ID_NAV_J:
+		case ID_NAV_K:
+		case ID_NAV_L:
+		case ID_NAV_M:
+		case ID_NAV_N:
+		case ID_NAV_O:
+		case ID_NAV_P:
+			curRoom = LOWORD(wparam) - ID_NAV_A;
+			PaintDunMap();
+			break;
+
+		case ID_RESET_ROOM_A:
+			resetRoomA = !resetRoomA;
+			if (resetRoomA)
+				CheckMenuItem( GetMenu(hwnd), ID_RESET_ROOM_A, MF_CHECKED);
+			else
+				CheckMenuItem( GetMenu(hwnd), ID_RESET_ROOM_A, MF_UNCHECKED);
+			break;
+
+		case ID_NAV_UP:
+			if (curLevel > 0)
+			{
+				CheckMenuItem( GetMenu(hwnd), ID_NAV_1 + curLevel, MF_UNCHECKED);
+				curLevel--;
+				CheckMenuItem( GetMenu(hwnd), ID_NAV_1 + curLevel, MF_CHECKED);
+				PaintDunMap();
+			}
+			break;
+
+		case ID_NAV_DOWN:
+			if (curLevel < 7)
+			{
+				CheckMenuItem( GetMenu(hwnd), ID_NAV_1 + curLevel, MF_UNCHECKED);
+				curLevel++;
+				CheckMenuItem( GetMenu(hwnd), ID_NAV_1 + curLevel, MF_CHECKED);
+				PaintDunMap();
+			}
+			break;
+
+		case ID_NAV_PREVRM:
+			if (curRoom > 0)
+				curRoom--;
+				PaintRoomMap();
+			break;
+
+		case ID_NAV_NEXTRM:
+			if (curRoom < 15)
+			{
+				curRoom++;
+				PaintRoomMap();
+			}
+			break;
+
+		case ID_OPTIONS_WRAPHALF:
+			wrapHalf = !wrapHalf;
+			if (wrapHalf)
+				CheckMenuItem( GetMenu(hwnd), ID_OPTIONS_WRAPHALF, MF_CHECKED);
+			else
+				CheckMenuItem( GetMenu(hwnd), ID_OPTIONS_WRAPHALF, MF_UNCHECKED);
+			break;
+		}
+	case WM_PAINT:
+		PaintDunMap();
+		PaintRoomMap();
+
+	}
+
+	return (DefWindowProc(hwnd, msg, wparam, lparam));
+
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow)
+{
+WNDCLASS winclass;	// this will hold the class we create
+
+HACCEL hAccelTable;
+MSG		 msg;		// generic message
+
+winclass.style			= CS_DBLCLKS | CS_OWNDC | 
+                          CS_HREDRAW | CS_VREDRAW;
+winclass.lpfnWndProc	= WindowProc;
+winclass.cbClsExtra		= 0;
+winclass.cbWndExtra		= 0;
+winclass.hInstance		= hInstance;
+winclass.hIcon			= LoadIcon(NULL, IDI_APPLICATION);
+winclass.hCursor		= LoadCursor(NULL, IDC_ARROW);
+winclass.hbrBackground	= (HBRUSH)GetStockObject(BLACK_BRUSH);
+winclass.lpszMenuName	= TEXT("MYMENU");
+winclass.lpszClassName	= WINDOW_CLASS_NAME;
+
+// register the window class
+if (!RegisterClass(&winclass))
+	return(0);
+
+// create the window
+	if (!(hwnd = CreateWindow(WINDOW_CLASS_NAME, // class
+							  "Ultima 5 Dungeon Simulator",	     // title
+							  WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+					 		  0,0,	   // x,y
+							  20*ICONSIZE, 16*ICONSIZE, // width, height
+							  NULL,	   // handle to parent 
+							  NULL,	   // handle to menu
+							  hInstance,// instance
+							  NULL)))	// creation parms
+	return(0);
+
+	localhdc = GetDC(hwnd);
+
+	HBITMAP roombmp = (HBITMAP)LoadImage(hInstance,"room.bmp",IMAGE_BITMAP,0,0,LR_LOADFROMFILE);
+	HBITMAP levelbmp = (HBITMAP)LoadImage(hInstance,"level.bmp",IMAGE_BITMAP,0,0,LR_LOADFROMFILE);
+	HBITMAP level2bmp = (HBITMAP)LoadImage(hInstance,"level2.bmp",IMAGE_BITMAP,0,0,LR_LOADFROMFILE);
+
+	roomdc = CreateCompatibleDC(localhdc);
+	leveldc = CreateCompatibleDC(localhdc);
+	level2dc = CreateCompatibleDC(localhdc);
+
+	HBITMAP oldroom = (HBITMAP)SelectObject(roomdc, roombmp);
+	HBITMAP oldlevel = (HBITMAP)SelectObject(leveldc, levelbmp);
+	HBITMAP oldlevel2 = (HBITMAP)SelectObject(level2dc, level2bmp);
+
+	ReadTheDungeons();
+	PaintDunMap();
+
+    hAccelTable = LoadAccelerators(hInstance, "MYACCEL");
+
+	CheckMenuItem( GetMenu(hwnd), ID_DUNGEON_DECEIT, MF_CHECKED);
+	CheckMenuItem( GetMenu(hwnd), ID_NAV_1, MF_CHECKED);
+
+	if (resetRoomA)
+		CheckMenuItem( GetMenu(hwnd), ID_RESET_ROOM_A, MF_CHECKED);
+
+	while (1)
+	{
+	if (PeekMessage(&msg,NULL,0,0,PM_REMOVE))
+		{ 
+		// test if this is a quit
+        if (msg.message == WM_QUIT)
+           break;
+	
+        if (!TranslateAccelerator(hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+		} // end if
+    
+	}
+	return 0;
+}
+
+void PaintDunMap()
+{
+	int i = 0;
+	int j = 0;
+
+	if (wrapHalf)
+	{
+		for (j=0; j < 8; j++)
+		{
+			for (i=0; i < 8; i++)
+			{
+				BitBlt(localhdc, i*16, j*16, 16, 16, level2dc,
+					16*(dunTile[i][j][curLevel][curDungeon] % 0x10), 16*(dunTile[i][j][curLevel][curDungeon] / 0x10), SRCCOPY);
+				BitBlt(localhdc, i*16+128, j*16, 16, 16, level2dc,
+					16*(dunTile[i][j][curLevel][curDungeon] % 0x10), 16*(dunTile[i][j][curLevel][curDungeon] / 0x10), SRCCOPY);
+				BitBlt(localhdc, i*16, j*16+128, 16, 16, level2dc,
+					16*(dunTile[i][j][curLevel][curDungeon] % 0x10), 16*(dunTile[i][j][curLevel][curDungeon] / 0x10), SRCCOPY);
+				BitBlt(localhdc, i*16+128, j*16+128, 16, 16, level2dc,
+					16*(dunTile[i][j][curLevel][curDungeon] % 0x10), 16*(dunTile[i][j][curLevel][curDungeon] / 0x10), SRCCOPY);
+			}
+		}
+		return;
+	}
+
+	for (j=0; j < 8; j++)
+	{
+		for (i=0; i < 8; i++)
+			BitBlt(localhdc, i*32, j*32, 32, 32, leveldc,
+				32*(dunTile[i][j][curLevel][curDungeon] % 0x10), 32*(dunTile[i][j][curLevel][curDungeon] / 0x10), SRCCOPY);
+	}
+}
+
+void PaintRoomMap()
+{
+	short i, j;
+	short tempIcon[11][11];
+
+	return;
+
+	if (curDungeon == 1)
+	{
+		Rectangle(localhdc, 288, 0, 640, 352);
+		return;
+	}
+	for (j=0; j < 11; j++)
+		for (i=0; i < 11; i++)
+			tempIcon[i][j] = dunTile[i][j][curLevel][curDungeon];
+
+	for (j=0; j < 11; j++)
+		for (i=0; i < 11; i++)
+			BitBlt(localhdc, i*32+288, j*32, 32, 32, roomdc,
+				32*(tempIcon[i][j] % 10), 32*(tempIcon[i][j] & 0xf), SRCCOPY);
+}
+
+void ReadTheDungeons()
+{
+	short tempRoom[32][11];
+
+	short i, j, k, l;
+
+	FILE * F = fopen("DUNGEON.DAT", "rb");
+
+	for (l=0; l < 8; l++)
+		for (k=0; k < 8; k++)
+			for (j=0; j < 8; j++)
+				for (i=0; i < 8; i++)
+					dunTile[i][j][k][l] = fgetc(F);
+
+	fclose(F);
+
+	F = fopen("DUNGEON.OVL", "rb");
+
+	for (l=0; l < 8; l++)
+		for (k=0; k < 16; k++)
+		{
+			for (j=0; j < 11; j++)
+				for (i=0; i < 32; i++)
+					tempRoom[i][j] = fgetc(F);
+			for (j=0; j < 11; j++)
+				for (i=0; i <11; i++)
+					roomBase[i][j][k][l] = tempRoom[i][j];
+		}
+}
+
+short curDirValid()
+{
+	return 0;
+}
+
+void adjustSecretCheckmarks()
+{
+}
---
+++ u5map/u5map.h
@@ -0,0 +1,77 @@
+#define ID_DUNGEON_DECEIT 101
+#define ID_DUNGEON_DESPISE 102
+#define ID_DUNGEON_DESTARD 103
+#define ID_DUNGEON_WRONG 104
+#define ID_DUNGEON_COVETOUS 105
+#define ID_DUNGEON_SHAME 106
+#define ID_DUNGEON_HYTHLOTH 107
+#define ID_DUNGEON_DOOM 108
+
+#define ID_DUNGEON_PREV 110
+#define ID_DUNGEON_NEXT 111
+
+#define ID_NAV_UP 201
+#define ID_NAV_DOWN 202
+#define ID_NAV_PREVRM 203
+#define ID_NAV_NEXTRM	204
+
+#define ID_NAV_1	211
+#define ID_NAV_2	212
+#define ID_NAV_3	213
+#define ID_NAV_4	214
+#define ID_NAV_5	215
+#define ID_NAV_6	216
+#define ID_NAV_7	217
+#define ID_NAV_8	218
+
+#define ID_NAV_NULL	220
+#define ID_NAV_A	221
+#define ID_NAV_B	222
+#define ID_NAV_C	223
+#define ID_NAV_D	224
+#define ID_NAV_E	225
+#define ID_NAV_F	226
+#define ID_NAV_G	227
+#define ID_NAV_H	228
+#define ID_NAV_I	229
+#define ID_NAV_J	230
+#define ID_NAV_K	231
+#define ID_NAV_L	232
+#define ID_NAV_M	233
+#define ID_NAV_N	234
+#define ID_NAV_O	235
+#define ID_NAV_P	236
+
+#define ID_RESET_ROOM_A	240
+
+#define ID_OPTIONS_WRAPHALF 300
+
+#define VK_0 0x30
+#define VK_1 0x31
+#define VK_2 0x32
+#define VK_3 0x33
+#define VK_4 0x34
+#define VK_5 0x35
+#define VK_6 0x36
+#define VK_7 0x37
+#define VK_8 0x38
+#define VK_9 0x39
+
+#define VK_A 0x41
+#define VK_B 0x42
+#define VK_C 0x43
+#define VK_D 0x44
+#define VK_E 0x45
+#define VK_F 0x46
+#define VK_G 0x47
+#define VK_H 0x48
+#define VK_I 0x49
+#define VK_J 0x4a
+#define VK_K 0x4b
+#define VK_L 0x4c
+#define VK_M 0x4d
+#define VK_N 0x4e
+#define VK_O 0x4f
+#define VK_P 0x50
+
+#define VK_MINUS 0xbd
